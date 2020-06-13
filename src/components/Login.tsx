bba7bb9955666523d1b8a50dc81e2d1b048a2ead
Sulejman <3107367+Sulejman@users.noreply.github.com>
2020-06-13 22:51:57 +0200

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,76 +5,76 @@ import { getUser } from '../selectors/LoginSelector'
 import { useHistory } from 'react-router'
 
 export const Login: React.FC = () => {
- const dispatch = useDispatch()
- const history = useHistory()
+  const dispatch = useDispatch()
+  const history = useHistory()
 
- const [username, setUsername] = useState('')
- const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
- const onLogin = () => {
-  dispatch(doUserLogin({ username, password }))
-  console.log('dispatch')
- }
+  const onLogin = () => {
+    dispatch(doUserLogin({ username, password }))
+    console.log('dispatch')
+  }
 
- const alreadyHasAccount = () => {
-  history.push('/')
- }
+  const alreadyHasAccount = () => {
+    history.push('/')
+  }
 
- return (
-  <div className="flex justify-center bg-field ml-12 mr-12 mt-6">
-   <form className="w-full max-w-sm mt-2">
-    <div className="text-white m-5 text-center leading-none tracking-tight">
-     SIGN IN{' '}
-    </div>
-    <div className="ml-4 mr-4">
-     <div className="flex items-center mb-3 login-field h-12 ">
-      <div className="w-1/3">
-       <label className="block text-bg-btn-login ml-5 text-xs leading-none tracking-tight">
-        Username
+  return (
+    <div className="flex justify-center bg-field ml-12 mr-12 mt-6">
+      <form className="w-full max-w-sm mt-2">
+        <div className="text-white m-5 text-center leading-none tracking-tight">
+          SIGN IN{' '}
+        </div>
+        <div className="ml-4 mr-4">
+          <div className="flex items-center mb-3 login-field h-12 ">
+            <div className="w-1/3">
+              <label className="block text-bg-btn-login ml-5 text-xs leading-none tracking-tight">
+                Email
        </label>
-      </div>
-      <div className="w-2/3 pt-3 pl-2">
-       <input
-        className="w-full login-field placeholder-white text-white text-xs focus:shadow-none active:shadow-none shadow-none"
-        type="text"
-        placeholder="damian@mediadrive"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-       />
-      </div>
-     </div>
-     <div className="flex items-center mb-3 login-field h-12">
-      <div className="w-1/3">
-       <label className="block text-bg-btn-login ml-5 text-xs leading-none tracking-tight">
-        Password
+            </div>
+            <div className="w-2/3 pt-3 pl-2">
+              <input
+                className="w-full login-field placeholder-white text-white text-xs focus:shadow-none active:shadow-none shadow-none"
+                type="text"
+                placeholder="damian@mediadrive"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex items-center mb-3 login-field h-12">
+            <div className="w-1/3">
+              <label className="block text-bg-btn-login ml-5 text-xs leading-none tracking-tight">
+                Password
        </label>
-      </div>
-      <div className="w-2/3 pt-3 pl-2">
-       <input
-        className="w-full login-field placeholder-white text-white text-xs focus:shadow-none active:shadow-none shadow-none"
-        type="text"
-        placeholder="***********"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-       />
-      </div>
-     </div>
-    </div>
-    <div className="text-center ml-10 mr-10 m-5">
-     <button
-      className="w-3/4 bg-options focus:outline-none text-white rounded leading-none tracking-tight h-10"
-      onClick={() => onLogin()}
-     >
-      Sign In
+            </div>
+            <div className="w-2/3 pt-3 pl-2">
+              <input
+                className="w-full login-field placeholder-white text-white text-xs focus:shadow-none active:shadow-none shadow-none"
+                type="password"
+                placeholder="***********"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="text-center ml-10 mr-10 m-5">
+          <button
+            className="w-3/4 bg-options focus:outline-none text-white rounded leading-none tracking-tight h-10"
+            onClick={() => onLogin()}
+          >
+            Sign In
      </button>
-     <div
-      className="text-white m-5 text-center leading-none tracking-tight cursor-pointer"
-      onClick={() => alreadyHasAccount()}
-     >
-      Create account?{' '}
-     </div>
+          <div
+            className="text-white m-5 text-center leading-none tracking-tight cursor-pointer"
+            onClick={() => alreadyHasAccount()}
+          >
+            Create account?{' '}
+          </div>
+        </div>
+      </form>
     </div>
-   </form>
-  </div>
- )
+  )
 }
